83d9d5410c9b77864fd50f055f3dffda4bf0ef4e
myunghoon3847-maker <myunghoon3847@gmail.com>
2026-07-14 15:08:57 +0900

    Delete harunihon_android_v1.0.1 directory

--- harunihon_android_v1.0.1/app/proguard-rules.pro
+++
@@ -1,1 +0,0 @@
-# No custom rules required for this WebView-based app.
--- harunihon_android_v1.0.1/gradlew.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set APP_HOME=%~dp0
-java -classpath "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
